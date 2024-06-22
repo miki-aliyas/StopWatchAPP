779f2eb3065b8d69cf31e56e33385081df0f0c6f
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2024-06-22 16:55:30 +0900

    ボタンのタップ処理を追加

--- app/src/main/java/com/example/stopwatchapp/MainActivity.java
+++ app/src/main/java/com/example/stopwatchapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.stopwatchapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView timerText;
+    private MaterialButton playButton, stopButton, refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        timerText = findViewById(R.id.timer_text);
+        playButton = findViewById(R.id.play_button);
+        stopButton = findViewById(R.id.stop_button);
+        refreshButton = findViewById(R.id.refresh_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("tag", "onclickが呼ばれたよ");
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("tag", "onclickが呼ばれたよ");
+            }
+        });
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("tag","onclickが呼ばれたよ");
+            }
         });
     }
 }
